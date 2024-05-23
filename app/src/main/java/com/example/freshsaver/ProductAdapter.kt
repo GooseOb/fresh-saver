6e499a7ad18ece2a596e5ca3e2332b534a219f6a
KrilMax <kirill2446@gmail.com>
2024-05-23 07:19:14 +0200

    Fixed bug with expire day

--- app/src/main/java/com/example/freshsaver/ProductAdapter.kt
+++ app/src/main/java/com/example/freshsaver/ProductAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.concurrent.TimeUnit
 
 class ProductAdapter(private val productList: List<Product>, private val onItemClick: (Product) -> Unit) :
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
@@ -12,11 +13,21 @@ class ProductAdapter(private val productList: List<Product>, private val onItemC
     class ProductViewHolder(itemView: View, val onItemClick: (Product) -> Unit) : RecyclerView.ViewHolder(itemView) {
         val nameTextView: TextView = itemView.findViewById(R.id.product_name)
         val descriptionTextView: TextView = itemView.findViewById(R.id.product_description)
+        val expirationTextView: TextView = itemView.findViewById(R.id.product_expiration)
 
         fun bind(product: Product) {
             nameTextView.text = product.title ?: "No Title"
             descriptionTextView.text = "Cost: ${product.cost ?: "Unknown"}"
+            val daysLeft = calculateDaysLeft(product.expirationDate)
+            expirationTextView.text = "Days Left: $daysLeft"
             itemView.setOnClickListener { onItemClick(product) }
+        }
+
+        private fun calculateDaysLeft(expirationDate: Long): Long {
+            val currentTime = System.currentTimeMillis()
+            val timeDifference = expirationDate - currentTime
+            val daysLeft = TimeUnit.MILLISECONDS.toDays(timeDifference)
+            return if (daysLeft < 0) 0 else daysLeft
         }
     }
 
